d8326795e3045f3cb483e57407da85cb23421e01
haska12 <88626706+haska12@users.noreply.github.com>
2021-11-28 23:03:21 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+ let storege = ["sdfghj"]
+ const ELinput = document.getElementById("el-input")
+ const subnitbtn = document.getElementById("subnit-btn")
+const deletebtn = document.getElementById("delete-btn")
+const uarl = document.getElementById("storege")
+  const animation = document.getElementById("animation")
+
+function render(Leads){
+    let listItems = ""
+for (let i = 0; i < Leads.length; i++) {
+    listItems +=
+    `<br> ${Leads[i]} 
+    `
+}
+ uarl.innerHTML =  listItems
+}
+function clearlist(){
+    storege =[]
+    render(storege)
+}
+
+
+subnitbtn.addEventListener("click" ,function(){
+    storege.push(ELinput.value)
+render(storege)
+ELinput.value =""
+})
+
+deletebtn.addEventListener("click",function(){
+    clearlist()
+    ELinput.value =""
+})
+animation.addEventListener("click",function(){
+    this.addEventListener("animationstart")
+})
